874741a9306c5b10a0ffa9bc9f63b1a9be44c0e9
ChanJinhuyk <chanjinhuyk@gmail.com>
2025-02-12 10:27:31 +0100

    Add files via upload

--- heroku.js
+++ heroku.js
@@ -1,4 +1,4 @@
-/* Copyright (C) 2024 KG TECH.
+/* Copyright (C) 2024 EMMANUEL242
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 Raganork MD - Sourav KL11
